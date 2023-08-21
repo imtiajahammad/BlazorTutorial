d32054719925e4cb044ff5e200b5d3218eb73c2c
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-08-21 22:53:26 +0600

    Blazor event handling

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -5,12 +5,18 @@ using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages;
 
 public class EmployeeDetailsBase : ComponentBase
 {
     public Employee Employee { get; set; } = new Employee();
+    protected string Coordinates { get; set; }
+
+    protected string ButtonText { get; set; } = "Hide Footer";
+    protected string CssClass { get; set; } = null;
+
     [Inject]
     public IEmployeeService EmployeeService { get; set; }
 
@@ -21,5 +27,24 @@ public class EmployeeDetailsBase : ComponentBase
     {
         Id = Id ?? "1";
         Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+    }
+
+    protected void Mouse_Move(MouseEventArgs e)
+    {
+        Coordinates = $"X = {e.ClientX} Y= {e.ClientY}";
+    }
+
+    protected void Button_Click()
+    {
+        if(ButtonText == "Hide Footer")
+        {
+            ButtonText = "Show Footer";
+            CssClass = "HideFooter";
+        }
+        else
+        {
+            CssClass = null;
+            ButtonText = "Hide Footer";
+        }
     }
 }
